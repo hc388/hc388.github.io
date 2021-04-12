d3bccd8b727c55659eba20e3e7237c67cee540cf
Hrithik <hc388@njit.edu>
2021-04-12 12:27:40 -0400

    Feat: App.js setup complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSearchPage: false,
+            searching: false,
+            booksList: {
+                "currentlyReading": [],
+                "wantToRead": [],
+                "read": []
+            }
+        }
+    }
+
+    toggleScreen = () => {
+        this.setState(prevState => ({showSearchPage: !prevState.showSearchPage}))
+    }
+    render() {
+        return(
+            <div>
+                This is the app home page!
+            </div>
+        )
+    }
+
+
 }
 
-export default App;
+export default BooksApp;
