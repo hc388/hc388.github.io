4543dfa08fe763607f268935ff7c198fb989b902
Hrithik <hc388@njit.edu>
2021-04-22 23:31:12 -0400

    Updated with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import BooksApp from './App'
 import './index.css'
 
-ReactDOM.render(<BrowserRouter><BooksApp/></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(<React.StrictMode><HashRouter><BooksApp/></HashRouter></React.StrictMode>, document.getElementById('root'))
